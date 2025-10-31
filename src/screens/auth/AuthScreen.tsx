63d9048c1bb2ce19bf03075762f13682e3e596ef
dev1week <rondo2860@gmail.com>
2025-10-31 23:28:19 +0900

    set Auth Navigation

---
+++ src/screens/auth/AuthScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+function AuthScreen() {
+  return (
+    <SafeAreaView>
+      <Text>AuthScreen</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default AuthScreen;
